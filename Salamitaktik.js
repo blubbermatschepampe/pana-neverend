9bce0170d702715fd860fad857aadf4e7b51a46c
blubbermatschepampe <82392914+blubbermatschepampe@users.noreply.github.com>
2023-10-16 14:16:23 +0200

    Update Salamitaktik.js

--- Salamitaktik.js
+++ Salamitaktik.js
@@ -7,159 +7,168 @@ const Watt1_MQTT = 'mqtt.0.panasonic_heat_pump.s0.Watt.1';
 const Heat_Energy_Production_MQTT = 'mqtt.0.panasonic_heat_pump.main.Heat_Energy_Production';
 
 const Timer_MS = 10000;
-//const TimerOneSecond = 1000/10000;
-const TimerOneMinute = 1000*60/10000;
 
 //startup
-createState('javascript.0.VIS.cutpel', 0, {name: 'cut p electric'});
+createState('javascript.0.VIS.cutpel', 1, {name: 'cut p electric'});
+createState('javascript.0.VIS.cop', 0, {name: 'cop berechnen'});
+createState('javascript.0.VIS.output', "x", {name: 'x'});
+
 setInterval(f_statemachine, Timer_MS)
 var state = "state_komp2";		//starten mit eingeschwungenem Zustand fuer Restarts
-var wait1 = 1;
+var waitseconds = 0;
 var dT = 3;
-var begrenzung = 0;                 
+var begrenzung = 1;                 
 var T_Max = 42
-//wenn dies auf 1 steht versucht er die minimale Leistung anzufahren. Man kann es auf 0 setzen, dann kann man auch höher Leistungen fahren indem man SetZ1HeatRequestTemperature hochzieht
+var Z1HeatRequestTemperature_old = 0;
+//wenn dies auf 1 steht versucht er die minimale Leistung anzufahren. Man kann es auf 0 (über VIS) setzen, dann kann man auch höher Leistungen fahren indem man SetZ1HeatRequestTemperature hochzieht
 //bis zu T_Max versucht er dann mindestens die minimale Leistung zu halten.
 
 //var state = "state_komp1";		//starten mit eingeschwungenem Zustand =erste 30 Minuten fuer Restarts
-//var wait1 = 110;//15*TimerOneMinute;
+//var wait1 = 30*60;
 var s_outputold = "";
 
+//##### set volrauf soll ###############################
+function f_setvl( x )
+{
+    if ((x != Z1HeatRequestTemperature_old) && ( x < T_Max))
+    {
+        setState(SetZ1HeatRequestTemperature_MQTT, x);
+        Z1HeatRequestTemperature_old = x;
+    }
+}
 //##### statemachine ###############################
 function f_statemachine()
 {
     //console.log("------------------------------");
     let Z1HeatRequestTemperature = getState(SetZ1HeatRequestTemperature_MQTT).val;
-    let Z1HeatRequestTemperature_old = 0;
     let IS_Compressor_Freq = getState(Compressor_Freq_MQTT).val;
     let IS_Main_Outlet_Temp = getState(Main_Outlet_Temp_MQTT).val;
     let IS_Main_Inlet_Temp = getState(Main_Inlet_Temp_MQTT).val;
     let ThreeWay_Valve_State = getState(ThreeWay_Valve_State_MQTT).val;
     let Watt1 = getState(Watt1_MQTT).val;
     let Heat_Energy_Production= getState(Heat_Energy_Production_MQTT).val;
+    let Z1HeatRequestTemperature_new = 0;
+    Z1HeatRequestTemperature_new = Z1HeatRequestTemperature;
 
 	//#### for all #################################################
     if (ThreeWay_Valve_State == 1) {state = "state_ww";}
 
 	//#####################################################
-    if (state == "state_off")
+    switch (state) 
     {
-        if (IS_Compressor_Freq != 0) {state = "state_ww";}
-    }
-	//#####################################################
-    else if (state == "state_ww")
-    {
-        if (ThreeWay_Valve_State == 0) 
-		{ 
-			state = "state_komp1";
-            wait1 = 30*TimerOneMinute;
-		}
-    }
-	//#####################################################
-    else if (state == "state_komp1")
-	{
-        if (IS_Main_Outlet_Temp >= Z1HeatRequestTemperature + 1.5)
-		{
-			Z1HeatRequestTemperature = IS_Main_Outlet_Temp;
-			setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-            console.log("hier58+");
-		}
-        if (Z1HeatRequestTemperature - IS_Main_Inlet_Temp <= dT + 1)
-		{
-			Z1HeatRequestTemperature = IS_Main_Inlet_Temp + dT + 1;
-			setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-            console.log("hier5+");
-		}
-        if (IS_Compressor_Freq >= 26) {
-            if (Z1HeatRequestTemperature > IS_Main_Outlet_Temp){
-                Z1HeatRequestTemperature = IS_Main_Outlet_Temp;
-                setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-                console.log("hier70-");
+        case "state_off":
+            if (IS_Compressor_Freq != 0) {state = "state_ww";}
+        break;
+        //#####################################################
+        case "state_ww":
+            if (ThreeWay_Valve_State == 0) 
+            { 
+                state = "state_komp1";
+                waitseconds = 30*60;
             }
-        }
-        if (IS_Compressor_Freq == 0) {
-            state = "state_off";
-        }
+        break;
+        //#####################################################
+        case "state_komp1":
+            if (IS_Main_Outlet_Temp >= Z1HeatRequestTemperature_new + 1.5)
+            {
+                Z1HeatRequestTemperature_new = IS_Main_Outlet_Temp;
+                f_setvl(Z1HeatRequestTemperature_new);
+                console.log("hier75+");
+            }
+            if (Z1HeatRequestTemperature_new - IS_Main_Inlet_Temp <= dT + 1)
+            {
+                Z1HeatRequestTemperature_new = IS_Main_Inlet_Temp + dT + 1;
+                f_setvl(Z1HeatRequestTemperature_new);
+                console.log("hier81+");
+            }
+            if (IS_Compressor_Freq >= 26) {
+                if (Z1HeatRequestTemperature_new > IS_Main_Outlet_Temp){
+                    Z1HeatRequestTemperature_new = IS_Main_Outlet_Temp;
+                    f_setvl(Z1HeatRequestTemperature_new);
+                    console.log("hier87-");
+                }
+            }
+            if (IS_Compressor_Freq == 0) {
+                state = "state_off";
+            }
 
-        wait1--;
-        if (wait1<=0)
-        {
-		    state = "state_komp2";
-        }
-	}
-	//#####################################################
-    else if (state == "state_komp2")
-    {
-        if (IS_Compressor_Freq == 0) {
-            Z1HeatRequestTemperature = 24;
-            setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-            console.log("hier3-");
-            state = "state_off";
-        }
-        else if (IS_Compressor_Freq <= 20)
-        {
-            if  (IS_Main_Outlet_Temp >= Z1HeatRequestTemperature + 1.5) {
-                Z1HeatRequestTemperature = Z1HeatRequestTemperature + 1;
-                if (Z1HeatRequestTemperature < IS_Main_Outlet_Temp + 1.5) {
-                    console.log("hier98+");
-                }
-                if (Z1HeatRequestTemperature <= T_Max) {
-                    setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-                    console.log("hier102+");
-                }
+            if (waitseconds <= 0)
+            {
+                state = "state_komp2";
             }
-            if (Z1HeatRequestTemperature - IS_Main_Inlet_Temp < dT) {
-                //mindestens 4 Grad fuer DeltaT setzen
-                Z1HeatRequestTemperature = IS_Main_Inlet_Temp + dT;
-                console.log("hier109+");
-                if (Z1HeatRequestTemperature <= T_Max) {
-                    setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-                    console.log("hier112+");
-                }
+        break;
+        //#####################################################
+        case "state_komp2":
+            if (IS_Compressor_Freq == 0) {
+                Z1HeatRequestTemperature_new = 24;
+                f_setvl(Z1HeatRequestTemperature_new);
+                console.log("hier104-");
+                state = "state_off";
             }
-        }
-        else if (IS_Compressor_Freq >= 22)
-        {
-            if((begrenzung == 1)){
-                Z1HeatRequestTemperature_old = Z1HeatRequestTemperature;
-                if (IS_Main_Outlet_Temp - IS_Main_Inlet_Temp > dT ) {
-                    //mindestens dT Grad fuer DeltaT setzen
-                    Z1HeatRequestTemperature = IS_Main_Inlet_Temp + dT;
-
-                    if (Z1HeatRequestTemperature < IS_Main_Outlet_Temp - 1) {
-                        //höchstens 1 Grad unter IS_Outlet_T nicht tiefer
-                        Z1HeatRequestTemperature = IS_Main_Outlet_Temp - 1;
+            else if (IS_Compressor_Freq <= 20)
+            {
+                if  (IS_Main_Outlet_Temp >= Z1HeatRequestTemperature_new + 1.5) 
+                {
+                    Z1HeatRequestTemperature_new = Z1HeatRequestTemperature_new + 1;
+                    if (Z1HeatRequestTemperature_new < IS_Main_Outlet_Temp + 1.5) 
+                    {
+                        console.log("hier1148+");
                     }
+                    f_setvl(Z1HeatRequestTemperature_new);
+                        console.log("hier117+");
+                }
+                if (Z1HeatRequestTemperature_new - IS_Main_Inlet_Temp < dT) 
+                {
+                    //mindestens 4 Grad fuer DeltaT setzen
+                    Z1HeatRequestTemperature_new = IS_Main_Inlet_Temp + dT;
+                    console.log("hier123+");
+                    f_setvl(Z1HeatRequestTemperature_new);
+                }
+            }
+            else if (IS_Compressor_Freq >= 22)
+            {
+                if((begrenzung == 1))
+                {
+                    if (IS_Main_Outlet_Temp - IS_Main_Inlet_Temp > dT ) 
+                    {
+                        //mindestens dT Grad fuer DeltaT setzen
+                        Z1HeatRequestTemperature_new = IS_Main_Inlet_Temp + dT;
 
-                    Z1HeatRequestTemperature -= 0.25;
-                    console.log("hier128-");
-                    if ((Z1HeatRequestTemperature <= T_Max) && (Z1HeatRequestTemperature < Z1HeatRequestTemperature_old)) {
-                        setState(SetZ1HeatRequestTemperature_MQTT, Z1HeatRequestTemperature);
-                        console.log("hier137-");
+                        if (Z1HeatRequestTemperature_new < IS_Main_Outlet_Temp - 1) 
+                        {
+                            //höchstens 1 Grad unter IS_Outlet_T nicht tiefer
+                            Z1HeatRequestTemperature_new = IS_Main_Outlet_Temp - 1;
+                        }
+
+                        //Z1HeatRequestTemperature -= 0.25;
+                        if (waitseconds <= 0)
+                        {
+                            console.log("hier145-");
+                            f_setvl(Z1HeatRequestTemperature_new);
+                            waitseconds=10*60;
+                        }
                     }
                 }
             }
-        }
-        else
-        {
-            console.log("hier138");
-        }
+        break;
+        //#####################################################
     }
-	//#####################################################
-    else if (state == "state_wait")
-    {
-        wait1 -= Timer_MS;
-        if (wait1 <= 0) { state = "state_komp"}
+        
+    waitseconds -= (Timer_MS/1000);
+    if (waitseconds <= 0) { 
+        waitseconds = 0;
     }
     
-    let s_output = "IS_Compressor_Freq="+IS_Compressor_Freq +" IS_Inlet_T="+IS_Main_Inlet_Temp +" IS_Outlet_T="+IS_Main_Outlet_Temp +" Z1HeatRequestTemperature="+Z1HeatRequestTemperature +" 3="+ ThreeWay_Valve_State +" wait1="+wait1 +" state="+state;
-    s_output += " dT=" + (IS_Main_Outlet_Temp - IS_Main_Inlet_Temp) + " abst=" + (Z1HeatRequestTemperature - IS_Main_Outlet_Temp)
+    let s_output = "IS_Compressor_Freq="+IS_Compressor_Freq +" IS_Inlet_T="+IS_Main_Inlet_Temp +" IS_Outlet_T="+IS_Main_Outlet_Temp +" 3="+ ThreeWay_Valve_State +" wait="+waitseconds;
+    s_output += " dT=" + (IS_Main_Outlet_Temp - IS_Main_Inlet_Temp) + " abst=" + (Z1HeatRequestTemperature_new - IS_Main_Outlet_Temp)+" state="+state +" VL_new="+Z1HeatRequestTemperature_new;
+    s_output += " VL="+Z1HeatRequestTemperature;
     if (s_outputold != s_output)
     {
         s_outputold = s_output;
         console.log(s_output);
-        console.log("Watt1=" + Watt1 + " COP=" + Heat_Energy_Production/Watt1 + " begrenzung=" + begrenzung);
-        //setState('mqtt.0_userdata.0.COP_heizen_nr',Heat_Energy_Production/Watt1);
+        console.log("Watt1=" + Watt1 + " COP=" + (Heat_Energy_Production/Watt1).toFixed(2) + " begrenzung=" + begrenzung);
+        setState('javascript.0.VIS.cop', (Heat_Energy_Production/Watt1).toFixed(2));
+        setState('javascript.0.VIS.output', s_output);
     }
        
 }
